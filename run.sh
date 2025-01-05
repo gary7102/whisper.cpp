eae67aad1b085b0964389cd3809252d7faf5405b
Gary Chen <160698915+gary7102@users.noreply.github.com>
2025-01-05 17:50:00 +0800

    update run.sh, make it perfect

--- run.sh
+++ run.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-# 0) rm old .wav and .txt
-# rm transcript.wav
-# rm transcript.wav.txt
+# 0) 刪除transcript.wav , transcript.wav.txt
+#rm transcript.wav
+#rm transcript.wav.txt
 
 # 1) 錄音：按下空白鍵或 Ctrl+C 手動中斷
-#echo "開始錄音... (按 Ctrl+C 或空白鍵中斷)"
-#arecord -D hw:3,0 -f S16_LE -c 1 -r 16000 transcript.wav
+echo "開始錄音... (按 Ctrl+C 結束)"
+arecord -D hw:3,0 -f S16_LE -c 1 -r 16000 transcript.wav
 
 # 2) (可選) 若需要轉成單聲道 16kHz
-# ffmpeg -i transcript.wav -ac 1 -ar 16000 transcript.wav
+echo "開始轉檔成channel 1 且 sample rate 16Khz"
+#ffmpeg -i transcript_before.wav -ac 1 -ar 16000 test_mono.wav
 # 如果已經是單聲道/16kHz，可跳過此步
 
 # 3) Whisper.cpp 語音轉文字
-#./build/bin/main \
-#  -m ./models/ggml-tiny.bin \
-#  -f transcript.wav \
-#  -otxt --no-timestamps
+#  -otxt 會將transcript.wav的內容寫入transcript.wav.txt
+echo "語音轉換為文字：..."
+./build/bin/main \
+  -m ./models/ggml-tiny.bin \
+  -f transcript.wav -l zh  \
+  -otxt --no-timestamps
 
-#TRANSCRIPT=$(cat transcript.wav.txt)
-TRANSCRIPT=$(cat test_mono.wav.txt)
-echo "Input：$TRANSCRIPT"
+
+TRANSCRIPT=$(cat transcript.wav.txt)
+# echo "Input ：$TRANSCRIPT"
+echo
 
 # 4) 呼叫 LM Studio API
 #   以 chat 形式提交
@@ -28,21 +32,22 @@ echo "Input：$TRANSCRIPT"
 #         如果是 /v1/chat/completions endpoint，則用 ChatCompletion 格式
 #   下例以 ChatCompletion 為範例
 #   curl -s : 為隱藏傳輸進度，可以刪掉-s 來看進度條
-#   POST http://........ 為 lm studio 之 server ip + api 
+#   POST http://........ 為 lm studio 之 server ip + api
 
-curl -sN -X POST http://192.168.50.196:1234/v1/chat/completions \
+curl -sN -X POST http://172.20.10.8:1234/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d '{
     "model": "causallm-14b",
     "messages": [
-      {"role": "system", "content": "You are a chatbox"},
+      {"role": "system", "content": "You are a extremely smart answer chatbox"},
       {"role": "user", "content": "'"$TRANSCRIPT"'"}
     ],
     "temperature":0.7,
-    "max_tokens":25,
+    "max_tokens":100,
     "stream":true
     }' |
 
+# 5) 處理stream 回傳的json 資料
 
 while IFS= read -r line; do
 
@@ -63,3 +68,4 @@ while IFS= read -r line; do
     # -n 表示不自動換行，讓所有內容即時接續在同一行
     echo -n "$content"
 done
+
